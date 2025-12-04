4f1a3fdb611eafd7be6a935f7ede20d79f015e67
BroBoss64 <minecraftbrandon07@gmail.com>
2025-12-04 15:36:26 -0600

    project created

---
+++ src/main/java/broboss64/totem_of_resurrection/TotemOfResurrectionDataGenerator.java
@@ -0,0 +1,11 @@
+package broboss64.totem_of_resurrection;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TotemOfResurrectionDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
